23f021915aee4d87ffc51bb7b429840d6834662f
Bruno Gavloski <gavloski11@gmail.com>
2024-12-13 19:51:15 -0300

    antes de instalar styled-components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import logo from './imagens/logo.svg'
+import Header from './componentes/Header'
+
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>Alura</strong> book</p>
-        </div>
-      </header>
+      <Header />
     </div>
   );
 }
